622ac1317f41f6e4027b197cb507418b1e0339aa
David <6957239+delta-G@users.noreply.github.com>
2023-10-28 14:12:27 -0500

    Moved in code for R4 boards

--- TimerOne.cpp
+++ TimerOne.cpp
@@ -19,9 +19,27 @@
 
 TimerOne Timer1;              // preinstatiate
 
+
+#if defined (ARDUINO_UNOR4_MINIMA) || defined (ARDUINO_UNOR4_WIFI)
+uint16_t TimerOne::reloadSetting = 0xFFFF;
+agt_cntsrc_t TimerOne::srcbits = PCLKB;
+uint8_t TimerOne::divbits = 0;
+uint8_t TimerOne::eventLinkIndex = 0;
+void (*TimerOne::isrCallback)() = TimerOne::isrDefaultUnused;
+
+void TimerOne::internalCallback(){
+  // Reset the interrupt link and the flag for the timer
+  R_ICU->IELSR[eventLinkIndex] &= ~(R_ICU_IELSR_IR_Msk);
+  R_AGT1->AGTCR &= ~(R_AGT0_AGTCR_TUNDF_Msk);
+  isrCallback();
+}
+
+#else
 unsigned short TimerOne::pwmPeriod = 0;
 unsigned char TimerOne::clockSelectBits = 0;
 void (*TimerOne::isrCallback)() = TimerOne::isrDefaultUnused;
+
+
 
 // interrupt service routine that wraps a user defined function supplied by attachInterrupt
 #if defined (__AVR_ATtiny85__)
@@ -57,3 +75,4 @@ void TimerOne::isr(void)
 void TimerOne::isrDefaultUnused()
 {
 }
+#endif
